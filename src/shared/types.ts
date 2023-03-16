8e2e26eb243afe92e84554bc7ec368d644f9e9ba
robedgar <robertiedgar@me.com>
2023-03-16 00:58:28 +0000

    complete

---
+++ src/shared/types.ts
@@ -0,0 +1,18 @@
+export enum SelectedPage {
+  Home = "home",
+  OurClasses = "ourclasses",
+  Benefits = "benefits",
+  ContactUs = "contactus",
+}
+
+export interface BenefitType {
+  icon: JSX.Element;
+  title: string;
+  description: string;
+}
+
+export interface ClassType {
+  name: string;
+  description?: string;
+  image: string;
+}
